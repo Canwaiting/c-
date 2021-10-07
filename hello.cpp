a4c5317d3ab5197396be7851431a4392698b6687
Canwaiting <455021735@qq.com>
2021-10-07 11:32:50 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 1;
+    cout << "Hello world" <<endl; 
+    cout << "this is my first line in linux" << endl;
+    cout << "look at the i =  ";
+    cout << i << endl;
+    return 0;
+}
