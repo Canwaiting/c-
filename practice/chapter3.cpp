0b480e7ef82216b76eb6257a43204004978df270
Canwaiting <455021735@qq.com>
2021-10-10 16:48:42 +0800

    1

--- practice/chapter3.cpp
+++ practice/chapter3.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
-int main(){
-    string s= "hello world";
-    for(auto c:s){
-        cout<<c<<endl;
-    }
+using std::vector;
+
+void printFunc(const vector<int>& v);
+void doubleFunc(vector<int>& v);
+
+int main(){ 
+    vector<int> v(10,5);
+    printFunc(v);
+    doubleFunc(v);
+    printFunc(v);
 
     return 0;
 
 }
 
+void printFunc(const vector<int>& v){ 
+    cout<<"the original"<<endl;
+    for(auto it = v.begin();it!=v.end();++it){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+}
+
+void doubleFunc(vector<int>& v){
+
+    for(auto it = v.begin();it!=v.end();++it){
+        *it *=2;
+    }
+}
