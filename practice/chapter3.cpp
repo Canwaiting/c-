5a1c0576ea4d416c1262cdafb78b0f68c78993db
Canwaiting <455021735@qq.com>
2021-10-10 19:29:07 +0800

    1

--- practice/chapter3.cpp
+++ practice/chapter3.cpp
@@ -11,24 +11,26 @@ using std::begin;
 using std::end;
 
 int main(){ 
-    int arr1[]={1,2,3};
-    int arr2[]={1,2,3};
-    int arr3[]={1,3,3};
-    if(compare(begin(arr1),begin(arr2),end(arr1),end(arr2))){
-        cout<<"same"<<endl;
-    }
-    else{
-        cout<<"not same"<<endl;
-    }
-    cout<<"=========="<<endl;
-    if(compare(begin(arr1),begin(arr3),end(arr1),end(arr3))){
-        cout<<"same"<<endl;
-    }
-    else{
-        cout<<"not same"<<endl;
-    }
+	int a[2] ={1,2};
+	int arr[3][4] = 
+    { 
+        { 0, 1, 2, 3 },
+        { 4, 5, 6, 7 },
+        { 8, 9, 10, 11 }
+    };
+	for(const int(&row)[4]:arr)
+		for(int col:row) cout<<col<<" ";
+	cout<<endl; 
+	cout<<"========="<<endl;
+	for(size_t i = 0;i!=3;++i)
+		for(size_t j = 0;j!=4;++j) cout<<arr[i][j]<<" ";
+	cout<<endl; 
+	cout<<"========="<<endl;
+	for(int (*row)[4] = arr;row!=arr+3;++row)
+		for(int *col = *row;col!=*row+4;++col) cout<<*col<<" ";
+	cout<<endl;
 
-
+	cout<<a[1]<<endl;
 
     return 0;
 
