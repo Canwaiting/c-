1e50468a3cd4b321a5ed678237262c9fdef1d53a
Canwaiting <455021735@qq.com>
2021-10-10 12:09:01 +0800

    morning

--- practice/chapter3.cpp
+++ practice/chapter3.cpp
@@ -5,14 +5,18 @@ using std::cin;
 using std::cout;
 using std::endl;
 int main(){
-    cout<<"hello world"<<endl;
-    string s;
-    //while(getline(cin,s)){
-    //    cout<<s<<endl;
-    //}
-    while(cin>>s){
-        cout<<s<<endl;
-    } 
+    //cout<<"hello world"<<endl;
+    string s="hello world";
+    string a;
+    string s1;
+    string s2;
+    //cin >> a;
+    //s>>a;
+    //cout<<s<<endl;
+    cin>>a;
+    //cin>>s1>>s2;
+    //cout<<s1<<s2<<endl;
+    cout<<a<<endl;
     return 0;
 
 }
