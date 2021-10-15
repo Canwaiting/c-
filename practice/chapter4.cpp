7a54af5ebf0fb5bb2b4a67b303241592dbedeb23
Canwaiting <455021735@qq.com>
2021-10-15 20:52:58 +0800

    1

--- practice/chapter4.cpp
+++ practice/chapter4.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
+
+
+void test(){
+    bool b;
+    int *ptr = new int;
+    cout<<"ptr="<<ptr<<endl;
+    int j=1,k=2;
+    int i=0;
+    cout<<i<<endl;
+    cout<<(j<k)<<endl;
+    if(i!=j<k){
+        cout<<"true"<<endl;
+    }
+    else{
+        cout<<"true"<<endl;
+    }
+
+}
+
+int main(){
+    test();
+    return 0;
+}
