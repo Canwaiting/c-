9a7c1864ff285189cd34ee6840314f9833915203
Canwaiting <455021735@qq.com>
2021-10-16 18:27:47 +0800

    1

--- practice/chapter4.cpp
+++ practice/chapter4.cpp
@@ -39,7 +39,44 @@ void test2(){
     cout<<"int size:"<<sizeof(*p)<<endl;
 }
 
+void test3(){
+    string text;
+    char ch;
+    char prech ='\0';
+    unsigned sum=0;
+    unsigned tab=0;
+    unsigned space=0;
+    unsigned nextline=0;
+    unsigned hello=0;
+    for(int i=0;i<5;i++){
+        cin>>std::noskipws>>ch;
+        switch(ch){ 
+            case 'A': sum++;break;
+            case 'E': sum++;break;
+            case 'I': sum++;break;
+            case 'O': sum++;break;
+            case 'U': sum++;break;
+            case 'a': sum++;break;
+            case 'e': sum++;break;
+            case 'i': sum++;break;
+            case 'o': sum++;break;
+            case 'u': sum++;break;
+            case ' ': space++;break;
+            case '\t': tab++;break;
+            case '\n': nextline++;break; 
+            case 'h': if(prech='e') ++hello;break;
+        }
+    }
+
+    cout<<"the sum is:"<<sum<<endl;
+    cout<<"the tab is:"<<tab<<endl;
+    cout<<"the space is:"<<space<<endl;
+    cout<<"the nextline is:"<<nextline<<endl;
+    cout<<"the hello is:"<<hello<<endl;
+
+}
+
 int main(){
-    test2();
+    test3();
     return 0;
 }
