2b6482b6ae00d472d0bd199c6c03d5eb08396cbe
Canwaiting <455021735@qq.com>
2021-11-17 16:00:14 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
+#include <netinet/in.h>
+#include <exception.h>
+
 using namespace std;
 
 class person{
 public:
-    person():age(10){}
+    person()throw(std::exception):age(10)
+    {
+        cout<<"the person initial function"<<endl;
+
+    }
 public:
     int age; 
 };
@@ -14,7 +21,7 @@ int main(){
     /*get person_age*/
     person* p1 = new person();
     cout<<"hello world"<<endl; 
-    cout<<"person_age:"<<p1->age<<endl;
+    //cout<<"person_age:"<<p1->age<<endl;
     return 0;
 }
 
