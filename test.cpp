04fb6bfb10ff58782d7a1c1338e91212c6af45a0
Canwaiting <455021735@qq.com>
2022-02-02 15:48:21 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,30 +1,12 @@
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
-#include <netinet/in.h>
-#include <exception.h>
+#include <netinet/in.h> 
 
 using namespace std;
 
-class person{
-public:
-    person()throw(std::exception):age(10)
-    {
-        cout<<"the person initial function"<<endl;
-        cout<<"hello world"<<endl;
-
-    }
-public:
-    int age; 
-};
-
 int main(){ 
-    /*get person_age*/
-    person* p1 = new person();
-    cout<<"hello world"<<endl; 
-    cout<<"this is the test for github"<<endl;
-    cout<<"the git for 2021-11-27"<<endl;
-    cout<<"the git for 2021-11-28"<<endl;
+    //std::cout<<"the git for 2021-11-28"<<std::endl;
     cout<<"the git for 2021-11-29"<<endl;
     //cout<<"person_age:"<<p1->age<<endl;
     return 0;
