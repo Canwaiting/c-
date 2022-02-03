37b4956d5f3e656ea4f2586b195c4de87468ffab
Canwaiting <455021735@qq.com>
2022-02-03 15:57:28 +0800

    1

--- test.cpp
+++ test.cpp
@@ -6,8 +6,11 @@
 using namespace std;
 
 int main(){ 
+    int a = 2;
+    int b = !a;
     //std::cout<<"the git for 2021-11-28"<<std::endl;
     cout<<"the git for 2021-11-29"<<endl;
+    cout<<"the b is:"<<b<<endl;
     //cout<<"person_age:"<<p1->age<<endl;
     return 0;
 }
