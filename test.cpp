e06f23dba0f6274a992d7253274b48e82a0d700a
Canwaiting <455021735@qq.com>
2022-02-03 16:41:09 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <iterator>
 #include <string.h>
 #include <netinet/in.h> 
 
 using namespace std;
 
 int main(){ 
-    int a = 2;
-    int b = !a;
-    //std::cout<<"the git for 2021-11-28"<<std::endl;
-    cout<<"the git for 2021-11-29"<<endl;
-    cout<<"the b is:"<<b<<endl;
-    //cout<<"person_age:"<<p1->age<<endl;
+    vector<int> ar = {-1,0,1,2,-1,-4};
+    //ar.push_back(100);
+
+    int temp = 0;
+      
+    // Declaring iterator to a vector
+    vector<int>::iterator ptr;
+    vector<int>::iterator ptr1;
+    vector<int>::iterator ptr2;
+    vector<int>::iterator ptr3;
+      
+    cout << "--------the original vector--------" << endl;
+    for (ptr1 = ar.begin(); ptr1 < ar.end(); ptr1++){
+        cout << *ptr1 << " ";
+    }
+    cout<<endl;
+    cout << "--------the original vector--------" << endl;
+    // Displaying vector elements using begin() and end()
+    for (ptr1 = ar.begin(); ptr1 < ar.end()-2; ptr1++)
+    { 
+
+        //the pointer 2
+        for(ptr2=ptr1+1;ptr2<ar.end()-1;ptr2++){ 
+            //the pointer 3
+            for(ptr3=ptr2+1;ptr3<ar.end();ptr3++){ 
+                temp = *ptr1 + *ptr2 + *ptr3; 
+                if(temp==0){ 
+                    cout << *ptr1 << " " << *ptr2 << " " << *ptr3 <<" = "<<temp<<endl;
+                    vector<int> res = {*ptr1,*ptr2,*ptr3};
+                    for (ptr = res.begin(); ptr < res.end(); ptr++){
+                        cout << *ptr << " ";
+                    } 
+                    cout<<endl;
+                }
+            }
+        }
+    }
+
+
     return 0;
 }
 
