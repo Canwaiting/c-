b0190dd5c77cc55cde90075910773a5bce2bfc5b
Canwaiting <455021735@qq.com>
2022-02-04 14:59:47 +0800

    1

--- test.cpp
+++ test.cpp
@@ -12,18 +12,21 @@ using namespace std;
 void function_3(){
     int same;
     vector<int> v{ 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+    vector<int> merge;
     vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    same = strs[0];
     
-    vector<string> testing;
-    if(testing==null){
-        cout<<"that is null"<<endl;
-    }
 
     //cout<<"the size:"<<strs.size()<<endl;
     cout<<strs[0]<<endl;
+    cout<<v.back()<<endl;
+    cout<<"strs"<<!strs.size()<<endl;
 }
 
+void function_4(){
+    vector<vector<int>> intervals = {{1,3},{2,3},{2,3},{2,3},{2,3},{2,3}};
+    cout<<intervals[0][0]<<endl;
+
+}
 
 int main(){ 
     function_3();
