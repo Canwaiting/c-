c1363a9fc2ed723b8a62b97cf6b59e8e17f25a31
Canwaiting <455021735@qq.com>
2022-02-06 16:03:01 +0800

    1

--- test.cpp
+++ test.cpp
@@ -73,16 +73,16 @@ int main(){
 
 
 class Solution {
-public:
-    bool check(TreeNode *p, TreeNode *q) {
-        if (!p && !q) return true;
-        if (!p || !q) return false;
-        return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
     }
+}
 
-    bool isSymmetric(TreeNode* root) {
-        return check(root, root);
-    }
-};
 
 
