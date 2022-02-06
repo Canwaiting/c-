6a44c78686d4760406c0b064af4684deeb613590
Canwaiting <455021735@qq.com>
2022-02-06 17:44:15 +0800

    1

--- test.cpp
+++ test.cpp
@@ -72,17 +72,20 @@ int main(){
 }
 
 
+
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if(root == null) {
-            return 0;
-        } else {
-            int left = maxDepth(root.left);
-            int right = maxDepth(root.right);
-            return Math.max(left, right) + 1;
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
         }
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
     }
-}
+};
 
 
 
