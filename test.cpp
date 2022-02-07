827183b56022058bd54a0c7b38d758863206b6a3
Canwaiting <455021735@qq.com>
2022-02-07 20:27:40 +0800

    1

--- test.cpp
+++ test.cpp
@@ -43,3 +43,20 @@ struct ListNode {
 int main(){ 
     return 0;
 }
+
+class Solution {
+    public:
+	int majorityElement(vector<int>& nums) {
+		unordered_map<int, int> counts;
+		int majority = 0, cnt = 0;
+		for (int num: nums) {
+			++counts[num];
+			if (counts[num] > cnt) {
+				majority = num;
+				cnt = counts[num];
+			}
+		}
+		return majority;
+	}
+};
+
