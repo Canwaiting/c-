8fa5edaa3b04fbba551b7f2e13001616143c98cd
Canwaiting <455021735@qq.com>
2022-02-07 18:24:42 +0800

    1

--- test.cpp
+++ test.cpp
@@ -55,20 +55,27 @@ int main(){
 
 class Solution {
 public:
-    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if (t1 == nullptr) {
-            return t2;
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector <vector <int>> ret;
+        if (!root) {
+            return ret;
         }
-        if (t2 == nullptr) {
-            return t1;
+
+        queue <TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int currentLevelSize = q.size();
+            ret.push_back(vector <int> ());
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                auto node = q.front(); q.pop();
+                ret.back().push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
         }
-        auto merged = new TreeNode(t1->val + t2->val);
-        merged->left = mergeTrees(t1->left, t2->left);
-        merged->right = mergeTrees(t1->right, t2->right);
-        return merged;
+        
+        return ret;
     }
 };
-
-
 
 
