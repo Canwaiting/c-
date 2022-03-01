3c986fbb40af45acbbd70b4e6c8b7d3b1454b653
Canwaiting <455021735@qq.com>
2022-03-01 13:05:19 +0800

    1

--- test.cpp
+++ test.cpp
@@ -72,10 +72,27 @@ bool testsort(){
     }
     cout<<endl;
 }
+// alignas 生效的情况
+
+struct Info {
+  uint8_t a;
+  uint16_t b;
+  uint8_t c;
+};
+
+
+struct alignas(4) Info2 {
+  uint16_t a;
+  uint16_t b;
+  uint16_t c;
+};
 
 int main(){ 
-    string s1 = "hello";
-    cout<<s1[0]<<endl;
+    std::cout << sizeof(Info) << std::endl;   // 6  2 + 2 + 2
+    std::cout << alignof(Info) << std::endl;  // 2
+    cout<<"-----------"<<endl;
+    std::cout << sizeof(Info2) << std::endl;   // 8  4 + 4
+    std::cout << alignof(Info2) << std::endl;  // 4
 
     return 0;
 }
